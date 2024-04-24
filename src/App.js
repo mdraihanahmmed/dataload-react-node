50fb0aa2b03d4d9cb31478910c0e71fb0ad83afe
MD.Raihan Ahmmed Sumon Howlader <127126602+mdraihanahmmed@users.noreply.github.com>
2024-04-24 15:34:09 +0600

    react dataload from node

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+  const emailRef = useRef();
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUsers = (e) => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUsers = { name, email };
+
+    fetch("http://localhost:5000/users", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUsers),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const addUsers = data;
+        const newUsers = [...users, addUsers];
+        setUsers(newUsers);
+      });
+
+    nameRef.current.value = "";
+    emailRef.current.value = "";
+
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>this is all data: {users.length}</h1>
+
+      <form onSubmit={handleAddUsers}>
+        <input type="text" ref={nameRef} name="" id="" placeholder="name" />
+        <input type="email" ref={emailRef} name="" id="" placeholder="email" />
+        <input type="submit" value="submit" />
+      </form>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            id: {user.id} name: {user.name} email: {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
